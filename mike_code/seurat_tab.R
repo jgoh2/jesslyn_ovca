707e26bbab1c52759ccbb8daa9d452e4d487939c
Mike Cuoco <mcuoco12@gmail.com>
2020-05-25 16:58:28 -0400

    added summary tables with custom function

---
+++ mike_code/seurat_tab.R
@@ -0,0 +1,58 @@
+# Create table from seurat object
+# Date: 05/25/2020
+# Author: Mike Cuoco
+
+library(tidyverse)
+library(reshape2)
+library(gt)
+
+seurat_tab <- function(seurat_object, col_var, row_var, group_var=NULL, title){
+  if (is.null(group_var)){
+    form = formula(paste0("total + ",row_var," ~ ",col_var))
+    tab = seurat_object[[]] %>%
+      group_by(!!sym(col_var), !!sym(row_var)) %>% 
+      tally() %>%
+      ungroup() %>%
+      ddply(c(as_label(sym(row_var))), mutate, total = sum(n)) %>%
+      dcast(form, value.var = "n", sum, fill=0) %>%
+      gt(rowname_col = row_var) %>%
+      tab_header(title = title, subtitle = paste("dataset:",seurat_object@project.name)) %>%
+      cols_move_to_end("total") %>%
+      summary_rows(fns = list(total = "sum"), decimals = 0) %>%
+      tab_options(
+        summary_row.background.color = "#ACEACE",
+        grand_summary_row.background.color = "#ACEACE",
+        heading.title.font.size = 25,
+        heading.title.font.weight = "bold",
+        column_labels.background.color = "#B4DCF7",
+        column_labels.font.size = 20,
+        column_labels.font.weight = "bold"
+      )
+  }
+  else {
+    form = formula(paste0("total + ",group_var," + ",row_var," ~ ",col_var))
+    tab = seurat_object[[]] %>%
+            group_by(!!sym(col_var), !!sym(row_var), !!sym(group_var)) %>% 
+            tally() %>%
+            ungroup() %>%
+            ddply(c(as_label(sym(row_var)), as_label(sym(group_var))), mutate, total = sum(n)) %>%
+            dcast(form, value.var = "n", sum, fill=0) %>%
+            group_by(!!sym(group_var)) %>%
+            gt(rowname_col = row_var) %>%
+            tab_header(title = title, subtitle =  paste("dataset:",seurat_object@project.name)) %>%
+            cols_move_to_end("total") %>%
+            summary_rows(groups = T, fns = list(total = "sum"), decimals = 0) %>%
+            grand_summary_rows(fns = list(`grand total` = "sum"), decimals = 0) %>%
+            tab_options(
+              summary_row.background.color = "#ACEACE",
+              grand_summary_row.background.color = "#ACEACE",
+              row_group.background.color = "#FFEFDB",
+              heading.title.font.size = 25,
+              heading.title.font.weight = "bold",
+              column_labels.background.color = "#B4DCF7",
+              column_labels.font.size = 20,
+              column_labels.font.weight = "bold"
+            )
+  }
+  return(tab)
+}
