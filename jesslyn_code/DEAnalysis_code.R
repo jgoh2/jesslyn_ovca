03456eada408769f18f36ad398809e3c04825283
jgoh2 <jgoh2@wellesley.edu>
2020-06-09 21:00:15 +0800

    [wip]HTAPP QC and PDX DE Analysis Updates

--- jesslyn_code/DEAnalysis_code.R
+++ jesslyn_code/DEAnalysis_code.R
@@ -23,8 +23,8 @@ DEAnalysis_code <- function(seurat_object, markers = NULL, group.by, group.1, gr
   if (graph == TRUE){
   #volcano plot for p values of ALL genes and label ALL GENES
   all = EnhancedVolcano(DEgenes, lab=rownames(DEgenes), x='avg_logFC', y='p_val_adj', 
-    pCutoff = 0.01, col = c("black", "black", "black", "red2"), 
-    title= paste(group.1, "vs", group.2, "DE Genes (lab all Genes)"), subtitle= "FC cutoff: 1.0, p cutoff: 0.01")
+    pCutoff = 0.05, FCcutoff = 0.5, col = c("black", "black", "black", "red2"), 
+    title= paste(group.1, "vs", group.2, "DE Genes (lab all Genes)"), subtitle= "LogFC cutoff: 0.5, p cutoff: 0.05")
   
   return (all)
   }
@@ -32,15 +32,15 @@ DEAnalysis_code <- function(seurat_object, markers = NULL, group.by, group.1, gr
   if(!(is.null(oxphosGenes))){
     #only label OXPHOS genes 
     all_lab_oxphos = EnhancedVolcano(DEgenes, lab= rownames(DEgenes), x='avg_logFC', y='p_val_adj', 
-        pCutoff = 0.01, col = c("black", "black", "black", "red2"), selectLab=oxphosGenes, 
-        title=paste(group.1, "vs", group.2, "DE Genes (lab oxphos genes)"), subtitle= "FC cutoff: 1.0, p cutoff: 0.01")
+        pCutoff = 0.05, FCcutoff = 0.5, col = c("black", "black", "black", "red2"), selectLab=oxphosGenes, 
+        title=paste(group.1, "vs", group.2, "DE Genes (lab oxphos genes)"), subtitle= "LogFC cutoff: 0.5, p cutoff: 0.05")
     
     #only graph OXPHOS genes
     DEgenes.oxphos = DEgenes %>% rownames_to_column("Gene") %>% filter(Gene %in% oxphosGenes) #filter oxphos genes
     DEgenes.oxphos = column_to_rownames(DEgenes.oxphos, "Gene")
     just_oxphos = EnhancedVolcano(DEgenes.oxphos,lab= rownames(DEgenes.oxphos), x='avg_logFC', y='p_val_adj', 
-        pCutoff = 0.01, col = c("black", "black", "black", "red2"), 
-        title=paste(group.1, "vs", group.2, "Oxphos DE Genes"), subtitle= "FC cutoff: 1.0, p cutoff: 0.01")
+        pCutoff = 0.05, FCcutoff = 0.5, col = c("black", "black", "black", "red2"), 
+        title=paste(group.1, "vs", group.2, "Oxphos DE Genes"), subtitle= "LogFC cutoff: 0.5, p cutoff: 0.05")
     
     return (all_lab_oxphos + just_oxphos)
   }
@@ -48,15 +48,15 @@ DEAnalysis_code <- function(seurat_object, markers = NULL, group.by, group.1, gr
   if(!(is.null(utrGenes))){
     #only label UTR genes 
     all_lab_utr = EnhancedVolcano(DEgenes, lab= rownames(DEgenes), x='avg_logFC', y='p_val_adj', 
-        pCutoff = 0.01, col = c("black", "black", "black", "red2"), selectLab=utrGenes, 
-        title=paste(group.1, "vs", group.2, "DE Genes (lab utr genes)"), subtitle= "FC cutoff: 1.0, p cutoff: 0.01")
+        pCutoff = 0.05, FCcutoff = 0.5, col = c("black", "black", "black", "red2"), selectLab=utrGenes, 
+        title=paste(group.1, "vs", group.2, "DE Genes (lab utr genes)"), subtitle= "LogFC cutoff: 0.5, p cutoff: 0.05")
     
     #only graph UTR genes
     DEgenes.utr = DEgenes %>% rownames_to_column("Gene") %>% filter(Gene %in% utrGenes) #filter utr genes
     DEgenes.utr = column_to_rownames(DEgenes.utr, "Gene")
     just_utr = EnhancedVolcano(DEgenes.utr,lab= rownames(DEgenes.utr), x='avg_logFC', y='p_val_adj', 
-        pCutoff = 0.01, col = c("black", "black", "black", "red2"), 
-        title=paste(group.1, "vs", group.2, "UTR DE Genes"), subtitle= "FC cutoff: 1.0, p cutoff: 0.01")
+        pCutoff = 0.05, FCcutoff = 0.5, col = c("black", "black", "black", "red2"), 
+        title=paste(group.1, "vs", group.2, "UTR DE Genes"), subtitle= "LogFC cutoff: 0.5, p cutoff: 0.05")
     
     return (all_lab_utr + just_utr)
   }
