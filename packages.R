9b8acd0c4e1baaf5a8bcc3e061616e46235606dc
Mike Cuoco <mcuoco12@gmail.com>
2020-07-14 23:44:44 -0400

    fix renv

--- packages.R
+++ packages.R
@@ -3,8 +3,6 @@
 if (!require(renv)) install.packages("renv"); library(renv)
 if (!require(BiocManager)) install.packages("BiocManager"); library(BiocManager)
 
-renv::init()
-
 # custom scripts
 source(here::here('code','DotPlot2.R'))
 source(here::here('code','DEAnalysis_code.R'))
