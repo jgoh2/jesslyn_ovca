e19473cb0eb243b1bf98774cbd33ec9fed57a0ee
Mike Cuoco <mcuoco12@gmail.com>
2020-07-14 22:19:14 -0400

    renv

--- packages.R
+++ packages.R
@@ -1,35 +1,9 @@
 # R packages --------------------------------------------------------------
 # pacman and BiocManager for easy loading/installation
-if (!require(pacman)) install.packages("pacman"); library(pacman)
+if (!require(renv)) install.packages("renv"); library(renv)
 if (!require(BiocManager)) install.packages("BiocManager"); library(BiocManager)
 
-# reproducibility tools
-p_load(renv)
-p_load(here)
-p_load(rrtools)
-p_load(workflowr)
-p_load(styler)
-
-# data analysis
-p_load(tidyverse)
-p_load(readxl)
-p_load(plyr)
-p_load(reshape2)
-p_load(Matrix)
-p_load(glue)
-p_load(broom)
-p_load(vroom)
-
-# biology
-p_load(msigdbr)
-p_load(Seurat)
-p_load(fgsea)
-
-# visualization
-p_load(gt)
-
-# other
-p_load(beepr)
+renv::init()
 
 # custom scripts
 source(here::here('code','DotPlot2.R'))
